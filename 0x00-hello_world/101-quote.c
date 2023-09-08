e2a9a6fe5415afef3d9ab71d58305310b920cb32
chialuka112 <cjluka2002@gmail.com>
2023-09-07 17:35:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- * main : Entry point
+ * main - Entry point
  * Return: Always 1 (success)
  */
 int main(void)
 {
 	write(2,
-	"and that place of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+	"and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
 	return (1);
 }
