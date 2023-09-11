b039330315baa112b74397d8e4529da7e4a81269
chialuka112 <cjluka2002@gmail.com>
2023-09-11 10:23:16 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * return: always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	while
+		(c <= '2') {
+			putchar (c);
+			c++;
+		}
+	putchar ('\n');
+	return (0);
+}
